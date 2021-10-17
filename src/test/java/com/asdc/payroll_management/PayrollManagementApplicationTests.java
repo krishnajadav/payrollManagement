e506009a2c6e88cbbd25e1cbea20fd300323f574
Ali Shan Khawaja <al290543@dal.ca>
2021-10-16 22:02:28 -0300

    New Employee Test Class with Class Exist Test Case

--- src/test/java/com/asdc/payroll_management/PayrollManagementApplicationTests.java
+++ src/test/java/com/asdc/payroll_management/PayrollManagementApplicationTests.java
@@ -11,16 +11,6 @@ class PayrollManagementApplicationTests {
     void contextLoads() {
     }
 
-  @Test
-  public void testEmployeeExist() {
-   //Calculator classUnderTest = new Calculator();
-   try {
-    Class C = Class.forName("com.asdc.payroll_management.Employee");
-    assertNotNull(C);
-   }catch (Exception e){
-    // System.out.println(e.getMessage());
-    assertNull(e);
-   }
-  }
+
 
 }
