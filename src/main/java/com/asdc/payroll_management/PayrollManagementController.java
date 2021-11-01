c28afe4c234171438963d1a01feea65b498e3c7c
jaswanth106 <jaswanth106@t9054.wpa.dal.ca>
2021-10-31 23:39:03 -0300

    Ticket Summary: Employee code generation and salary generation for new employees Ticket ID: https://asdc-group17.atlassian.net/browse/A1-12

--- src/main/java/com/asdc/payroll_management/PayrollManagementController.java
+++ src/main/java/com/asdc/payroll_management/PayrollManagementController.java
@@ -1,21 +1,7 @@
 package com.asdc.payroll_management;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.asdc.payroll_management.db.DBConnect;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PayrollManagementController {
-
-
-    @GetMapping("/{name}")
-    public String helloWorld(@PathVariable String name, @RequestParam(name = "last") String lastName) {
-        if(DBConnect.devDBConnection()) {
-        	return "Hey "+name+ " "+ lastName +"!";
-        }
-    	return "";
-    }
-
 }
