c7ba53808165469b58e495a9332ba425192936b3
mmandaliya <maan.mandaliya@dal.ca>
2021-12-07 14:48:57 -0400

    js changed

--- src/main/resources/static/js/addReimbursements.js
+++ src/main/resources/static/js/addReimbursements.js
@@ -1,80 +1,70 @@
 function initialize() {
-	
-    var request = $.ajax({
-        url: "http://localhost:8080/viewMyReimbursements",
-        type: "POST",
-        contentType: 'application/json; charset=utf-8',
-        data: {id:"1225"},
-        success: function(result) {
-            console.log(result);
-            var table = document.getElementById("fviewReimbursements");
-            for (var i = 0; i < result.length; i++) {
-                var row = table.insertRow(i);
-                var cell0 = row.insertCell(0);
-                var cell1 = row.insertCell(1);
-                var cell2 = row.insertCell(2);
-                var cell3 = row.insertCell(3);
-                var cell4 = row.insertCell(4);
 
-				let isAcceptedvalue="";
-                if(result[i]["isAccepted"]){
-                    isAcceptedvalue="Accepted";
-                }else{
-                    isAcceptedvalue="Pending";
-                }
+	var request = $.ajax({
+		url: "http://localhost:8080/viewMyReimbursements",
+		type: "POST",
+		contentType: 'application/json; charset=utf-8',
+		success: function(result) {
+			confirm(result[0]);
+			var table = document.getElementById("fviewReimbursements");
+			for (var i = 0; i < result.length; i++) {
+				var row = table.insertRow(i);
+				var cell0 = row.insertCell(0);
+				var cell1 = row.insertCell(1);
+				var cell2 = row.insertCell(2);
+				var cell3 = row.insertCell(3);
+				var cell4 = row.insertCell(4);
+
+				let isAcceptedvalue = "";
+				if (result[i]["isAccepted"]) {
+					isAcceptedvalue = result[i]["isAccepted"];
+				} else {
+					isAcceptedvalue = "Pending";
+				}
 				
-                cell0.innerHTML = result[i]["RR_TypeID"];
-                cell1.innerHTML = result[i]["RR_Note"];
-                cell2.innerHTML = result[i]["RR_Amount"];
-                cell3.innerHTML = result[i]["RR_Date"];
-                cell4.innerHTML = isAcceptedvalue;
+				
+				cell0.innerHTML = result[i]["rr_TypeID"];
+				cell1.innerHTML = result[i]["rr_Note"];
+				cell2.innerHTML = result[i]["rr_Amount"];
+				cell3.innerHTML = result[i]["rr_Date"];
+				cell4.innerHTML = isAcceptedvalue;
 
 
-            }
-        }
-    });
+			}
+		}
+	});
 }
 
 function postData() {
-    confirm(document.getElementById("femployeeID").value);
-    var employeeID = document.getElementById("femployeeID").value;
-    var reimbursementType = document.getElementById("freimbursementTypeID").value;
-    var reimbursementNote = document.getElementById("freimbursementNote").value;
-    var reimbursementAmount = document.getElementById("freimbursementAmount").value;
-    var reimbursementDate = new Date(document.getElementById("freimbursementDate").value).getTime();
+	var type = document.getElementById("freimbursementTypeID").value;
+	var note = document.getElementById("freimbursementNote").value;
+	var ammount = document.getElementById("freimbursementAmount").value;
+	var date = String(new Date(document.getElementById("freimbursementDate").value).getTime());
 
-
-
-    console.log('1');
-    var request = $.ajax({
-        url: "http://localhost:8080/getEmployeeReimbursements",
-        type: "POST",
-        contentType: 'application/json; charset=utf-8',
-        data: JSON.stringify({
-            'RR_EmployeeID': employeeID,
-            'RR_TypeID': reimbursementType,
-            'RR_Note': reimbursementNote,
-            'RR_Amount': reimbursementAmount,
-            'RR_Date': reimbursementDate,
-			'isAccepted' : false
-        }),
-        success: function(result) {
-            console.log("This is console");
-            console.log(result);
-            if(result){
-                var empcode = document.getElementById('femployeeID');
-                empcode.value = result.employeeID;
-                document.getElementById('femployeeID').disabled = true;
-                document.getElementById('freimbursementTypeID').disabled = true;
-                document.getElementById('freimbursementNote').disabled = true;
-                document.getElementById('freimbursementAmount').disabled = true;
-                document.getElementById('freimbursementDate').disabled = true;
-            } else{
-                var errordiv = document.getElementById('div-error-info');
-                errordiv.innerHTML = '<p style="color:red;">'+result.error+'</p>;';
-            }
-        }, error: function(err) {
-            console.log(err);
-        }
-    });
+	var request = $.ajax({
+		url: "http://localhost:8080/getReimbursementRequest",
+		type: "POST",
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify({
+			'RR_TypeID': type,
+			'RR_Note': note,
+			'RR_Amount': ammount,
+			'RR_Date': date
+		}),
+		success: function(result) {
+			if (result.error == null) {
+				document.getElementById('freimbursementNote').disabled = true;
+				document.getElementById('freimbursementTypeID').disabled = true;
+				document.getElementById('freimbursementAmount').disabled = true;
+				document.getElementById('freimbursementDate').disabled = true;
+				var errordiv = document.getElementById('div-error-info');
+				errordiv.innerHTML = "<p style='color:Green;'>Insert Successfully</p>;";
+			} else {
+				var errordiv = document.getElementById('div-error-info');
+				errordiv.innerHTML = "<p style='color:red;'>" + result.error + '</p>;';
+			}
+		}, error: function(err) {
+			console.log(err);
+		}
+	});
 }
--- src/main/resources/static/js/manageReimbursements.js
+++ src/main/resources/static/js/manageReimbursements.js
@@ -1,114 +1,117 @@
 function initialize() {
 
-    var request = $.ajax({
-        url: "http://localhost:8080/getAllStaffReimbursements",
-        type: "POST",
-        contentType: 'application/json; charset=utf-8',
-        data: "1226",
-        success: function(result) {
-            console.log(result);
-            var select = document.getElementById("fstaffID");
-            for (var i = 0; i < result.length; i++) {
-                select.options[select.options.length] = new Option(result[i]["Employee_ID"], i);
-            }
-        }
-    });
+	var request = $.ajax({
+		url: "http://localhost:8080//Manager/getAllStaff",
+		type: "POST",
+		contentType: 'application/json; charset=utf-8',
+		data: "1226",
+		success: function(result) {
+			console.log(result);
+			var select = document.getElementById("fstaffID");
+			for (var i = 0; i < result.length; i++) {
+				select.options[select.options.length] = new Option(result[i]["employee_ID"], i);
+			}
+		}
+	});
 }
 
 
-function postData(){
-    var select = document.getElementById("fstaffID");
-    var employeeID = select.options[select.selectedIndex].text;
-    console.log(employeeID)
+function postData() {
+	var select = document.getElementById("fstaffID");
+	var employeeID = select.options[select.selectedIndex].text;
+	console.log(employeeID)
 
-    var request = $.ajax({
-        url: "http://localhost:8080/viewAllStaffReimbursements",
-        type: "POST",
-        contentType: 'application/json; charset=utf-8',
-        data: employeeID,
-        success: function(result) {
-            console.log(result);
-            var table = document.getElementById("fviewReimbursements");
-            for (var i = 0; i < result.length; i++) {
-                var row = table.insertRow(i);
-                var cell0 = row.insertCell(0);
-                var cell1 = row.insertCell(1);
-                var cell2 = row.insertCell(2);
-                var cell3 = row.insertCell(3);
-                var cell4 = row.insertCell(4);
-
-                let lrId = result[i]["lr_ID"];
-                cell0.innerHTML = result[i]["RR_TypeID"];
-                cell1.innerHTML = result[i]["RR_Note"];
-                cell2.innerHTML = result[i]["RR_Amount"];
-                cell3.innerHTML = result[i]["RR_Date"];
-                let acceptRequestString = `"acceptRequest(${RR_ID})"`;
-                let denyRequestString = `"denyRequest(${RR_ID})"`;
-
-                cell4.innerHTML =
-                    "<input type=\"button\" id=\"faccept_"+result[i]["RR_ID"]+"\" data_id=\""+result[i]["RR_ID"]+"\" name=\"faccept\" onClick = "+acceptRequestString+" value=\"Accept\" />\n" +
-                    "            <br>\n" +
-                    "            <input type=\"button\" id=\"fdeny_"+result[i]["RR_ID"]+"\" data_id=\""+result[i]["RR_ID"]+"\" name=\"fdeny\" onClick = "+denyRequestString+" value=\"Deny\" />";
+	var request = $.ajax({
+		url: "http://localhost:8080/Manager/viewAllStaffReimbursements",
+		type: "POST",
+		contentType: 'application/json; charset=utf-8',
+		data: employeeID,
+		success: function(result) {
+			console.log(result);
+			var table = document.getElementById("fviewReimbursements");
+			for (var i = 0; i < result.length; i++) {
+				var row = table.insertRow(i);
+				var cell0 = row.insertCell(0);
+				var cell1 = row.insertCell(1);
+				var cell2 = row.insertCell(2);
+				var cell3 = row.insertCell(3);
+				var cell4 = row.insertCell(4);
 
 
-            }
-        }
-    });
+				let lrId = result[i]["rr_ID"];
+				cell0.innerHTML = result[i]["rr_TypeID"];
+				cell1.innerHTML = result[i]["rr_Note"];
+				cell2.innerHTML = result[i]["rr_Amount"];
+				cell3.innerHTML = result[i]["rr_Date"];
+				var acceptReqString = "'acceptRequest(" + result[i]["rr_ID"] + ")'";
+				var rejectReqString = "'denyRequest(" + result[i]["rr_ID"] + ")'";
+				let acceptRequestString = ``+acceptReqString+``;
+				let denyRequestString = ``+rejectReqString+``;
+
+				cell4.innerHTML =
+					"<input type=\"button\" id=\"faccept_" + result[i]["rr_ID"] + "\" data_id=\"" + result[i]["rr_ID"] + "\" name=\"faccept\" onClick = " + acceptRequestString + " value=\"Accept\" />\n" +
+					"            <br>\n" +
+					"            <input type=\"button\" id=\"fdeny_" + result[i]["rr_ID"] + "\" data_id=\"" + result[i]["rr_ID"] + "\" name=\"fdeny\" onClick = " + denyRequestString + " value=\"Deny\" />";
+
+
+			}
+		}
+	});
 
 
 }
 
 
-function denyRequest(RR_ID){
-    console.log(RR_ID);
-    let RR_IDString = RR_ID.toString();
+function denyRequest(RR_ID) {
+	console.log(RR_ID);
+	let RR_IDString = RR_ID.toString();
 
-    var request = $.ajax({
-        url: "http://localhost:8080/rejectReimbursementRequest",
-        type: "POST",
-        contentType: 'application/json; charset=utf-8',
-        data: RR_IDString,
-        success: function(result) {
-            console.log(result);
-            if(result){
-                document.getElementById('faccept_'+RR_ID).disabled = true;
-                document.getElementById('fdeny_'+RR_ID).disabled = true;
+	var request = $.ajax({
+		url: "http://localhost:8080/Manager/rejectReimbursementRequest",
+		type: "POST",
+		contentType: 'application/json; charset=utf-8',
+		data: RR_IDString,
+		success: function(result) {
+			console.log(result);
+			if (result) {
+				document.getElementById('faccept_' + RR_ID).disabled = true;
+				document.getElementById('fdeny_' + RR_ID).disabled = true;
 
 
-            } else{
-                var errordiv = document.getElementById('div-error-info');
-                errordiv.innerHTML = '<p style="color:red;">'+result.error+'</p>;';
-            }
-        }, error: function(err) {
-            console.log(err);
-        }
-    });
+			} else {
+				var errordiv = document.getElementById('div-error-info');
+				errordiv.innerHTML = '<p style="color:red;">' + result.error + '</p>;';
+			}
+		}, error: function(err) {
+			console.log(err);
+		}
+	});
 }
-function acceptRequest(RR_ID){
-    console.log(RR_ID);
-    let RR_IDString = RR_ID.toString();
+function acceptRequest(RR_ID) {
+	console.log(RR_ID);
+	let RR_IDString = RR_ID.toString();
 
-    var request = $.ajax({
-        url: "http://localhost:8080/acceptReimbursementRequest",
-        type: "POST",
-        contentType: 'application/json; charset=utf-8',
-        data: RR_IDString,
-        success: function(result) {
-            //console.log("This is console");
-            console.log(result);
+	var request = $.ajax({
+		url: "http://localhost:8080/Manager/acceptReimbursementRequest",
+		type: "POST",
+		contentType: 'application/json; charset=utf-8',
+		data: RR_IDString,
+		success: function(result) {
+			//console.log("This is console");
+			console.log(result);
 
-            if(result){
-                document.getElementById('fdeny_'+RR_ID).disabled = true;
-                document.getElementById('faccept_'+RR_ID).disabled = true;
+			if (result) {
+				document.getElementById('fdeny_' + RR_ID).disabled = true;
+				document.getElementById('faccept_' + RR_ID).disabled = true;
 
-            } else{
-                var errordiv = document.getElementById('div-error-info');
-                errordiv.innerHTML = '<p style="color:red;">'+result.error+'</p>;';
-            }
-        }, error: function(err) {
-            console.log(err);
-        }
-    });
+			} else {
+				var errordiv = document.getElementById('div-error-info');
+				errordiv.innerHTML = '<p style="color:red;">' + result.error + '</p>;';
+			}
+		}, error: function(err) {
+			console.log(err);
+		}
+	});
 }
 
 
